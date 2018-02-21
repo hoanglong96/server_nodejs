bdc0eefcca28328a53e1f94e67eedb997b3b4386
Hoàng Long <long11hq@gmail.com>
2018-02-21 09:17:56 +0700

    update 21/2

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ app.get('/getTuTrongNhom/:idNhomTu', function (req, res) {
     if (err) {
       res.json({ success: 0, message: "Could not get data from mlab"});
     } else {
-      res.send(tuvung);
+      res.send({tuvung :tuvung});
     }
   });
 });
